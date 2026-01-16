7e1487d648822262acb2020550ec5c2706eb26fe
kaiser3532 <kaisermalik3532@gmail.com>
2026-01-16 06:40:21 +0000

    Day 1: intro program

---
+++ intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"Hello, world!"<<endl;
+    cout<<"My name is kaiser."<<endl;
+      cout<<"I am learning C++ Proramming."<<endl;
+  cout<<"This is Day-1 of my coding journey."<<endl;
+    return 0;
+}
